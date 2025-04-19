f3bb48707c052e93cab9688137fc183bdf971846
UjiinEatingTangerines <1996.devjiinu@gmail.com>
2025-04-19 12:20:22 +0900

    feat: Exporter interface 생성 후, 구현체 파일 생성 (현재는 html 과 postman 사용 기능만)

---
+++ openapi-core/src/main/kotlin/io/github/openapidocstoolkit/core/export/HtmlExporter.kt
@@ -0,0 +1,12 @@
+package io.github.openapidocstoolkit.core.export
+
+class HtmlExporter: Exporter {
+
+    override fun supports(format: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun export(json: String, outputDir: String, version: String) {
+        TODO("Not yet implemented")
+    }
+}
