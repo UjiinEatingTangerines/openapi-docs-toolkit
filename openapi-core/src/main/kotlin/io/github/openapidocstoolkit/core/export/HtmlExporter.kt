5c718262f39ecaefac59bd3e82a7001d375adc4f
UjiinEatingTangerines <1996.devjiinu@gmail.com>
2025-04-19 14:53:39 +0900

    feat: Exporter supports 구현

--- openapi-core/src/main/kotlin/io/github/openapidocstoolkit/core/export/HtmlExporter.kt
+++ openapi-core/src/main/kotlin/io/github/openapidocstoolkit/core/export/HtmlExporter.kt
@@ -2,11 +2,9 @@ package io.github.openapidocstoolkit.core.export
 
 class HtmlExporter: Exporter {
 
-    override fun supports(format: String): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun supports(format: String): Boolean = format.equals("html", ignoreCase = true)
 
     override fun export(json: String, outputDir: String, version: String) {
-        TODO("Not yet implemented")
+
     }
 }
--- openapi-core/src/main/kotlin/io/github/openapidocstoolkit/core/export/RedocExporter.kt
+++ openapi-core/src/main/kotlin/io/github/openapidocstoolkit/core/export/RedocExporter.kt
@@ -1,8 +1,8 @@
 package io.github.openapidocstoolkit.core.export
 
-class PostmanExporter: Exporter{
+class RedocExporter: Exporter {
 
-    override fun supports(format: String): Boolean = format.equals("postman", ignoreCase = true)
+    override fun supports(format: String): Boolean = format.equals("redoc", ignoreCase = true)
 
     override fun export(json: String, outputDir: String, version: String) {
         TODO("Not yet implemented")
