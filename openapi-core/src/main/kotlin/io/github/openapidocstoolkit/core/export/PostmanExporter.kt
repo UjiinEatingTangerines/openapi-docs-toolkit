25ce081464927bc0766dd587ca9417c7b886ca2c
UjiinEatingTangerines <1996.devjiinu@gmail.com>
2025-05-10 17:53:59 +0900

    feat: Exporter supports 구현

--- openapi-core/src/main/kotlin/io/github/openapidocstoolkit/core/export/PostmanExporter.kt
+++ openapi-core/src/main/kotlin/io/github/openapidocstoolkit/core/export/PostmanExporter.kt
@@ -1,10 +1,15 @@
 package io.github.openapidocstoolkit.core.export
 
+import java.io.File
+
 class PostmanExporter: Exporter{
 
     override fun supports(format: String): Boolean = format.equals("postman", ignoreCase = true)
 
     override fun export(json: String, outputDir: String, version: String) {
-        TODO("Not yet implemented")
+        val fileName = "openapi-postman-$version.postman_collection.json"
+        val file = File(outputDir, fileName)
+        file.writeText(json)
+        println("✅ Postman collection exported to: ${file.absolutePath}")
     }
 }
